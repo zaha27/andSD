2cb443c75d2a14fb27fc822098b8764db6bf7864
zaha27 <andrei.zaharia2005@gmail.com>
2025-03-17 08:17:17 +0200

    top

--- laborator5/sablon.cpp
+++ laborator5/sablon.cpp
@@ -1,29 +1,39 @@
 #include <iostream>
 
-struct Stack {
+struct Node {
     int data;
-    Stack* succ;
+    Node* succ;
 };
 
-void push(Stack *&head, int value) {
-    Stack* p = new Stack();
+typedef Node* Stack;
+
+void push(Stack &head, int value) {
+    Stack p = new Node;
     p->data = value;
     p->succ = head;
     head = p;
 }
 
-bool isEmpty(Stack *&head) {  
+bool isEmpty(const Stack &head) {  
     return (head == nullptr);
 }
 
-bool pop(Stack *&head) {
+bool pop(Stack &head) {
     if (!isEmpty(head)) {  
-        Stack* p = head;  
+        Stack p = head;  
         head = head->succ;
         delete p;
         return true;
     } else {
         return false;
+    }
+}
+
+int top(const Stack &head) {
+    if(!isEmpty) {
+        return false;
+    } else {
+        return head -> data;
     }
 }
 
