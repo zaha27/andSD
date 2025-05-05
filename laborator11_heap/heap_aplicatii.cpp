a7a8ebd2b89c1d86bb622590c4ba9096c6bb1ba5
zaha27 <andrei.zaharia2005@gmail.com>
2025-05-05 09:23:34 +0300

    coomit

--- laborator11_heap/heap_aplicatii.cpp
+++ laborator11_heap/heap_aplicatii.cpp
@@ -7,11 +7,15 @@ void sortare_aplicatia1(int de_sortat[], int heap[], int n) {
     for(int i = 0; i < n; ++i) {
         insertHeap(heap, n, de_sortat[i]);
     }
-    
+
     int ret_val;
     do { 
         ret_val = removeHeap(heap, n);
         if(ret_val != -1)
             cout << ret_val << ' ';
     } while(ret_val != -1);
+}
+
+void sortare_aplicatia2(int de_sortat[], int heap[], int n) {
+    
 }
--- laborator11_heap/heap_build.cpp
+++ laborator11_heap/heap_build.cpp
@@ -3,3 +3,31 @@
 
 using namespace std;
 
+void build_heap_v1(int heap[], int &n) {
+    for(int i = 2; i <= n; ++i) {
+        insertHeap(heap, i - 1, heap[i]);
+    }
+}
+
+void build_heap_v2(int heap[], int &n) {
+    for(int i = n / 2; i >= 1; --i) {
+        retro(heap, n, i);
+    }
+}
+
+void retro(int heap[], int &n, int i) {
+    int parinte = i;
+    int fiu = 2 * i;
+    while(fiu <= n) {
+        if(fiu + 1 <= n && heap[fiu] < heap[fiu + 1]) {
+            fiu++;
+        }
+        if(heap[fiu] > heap[parinte]) {
+            swap(heap[parinte], heap[fiu]);
+            parinte = fiu;
+            fiu *= 2;
+        } else {
+            fiu = n + 1;
+        }
+    }
+}
